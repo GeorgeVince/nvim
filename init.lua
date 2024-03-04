9b9aa9a03c286097c7e29cad93e8ccd0d1f978cc
George <george.vince@hotmail.co.uk>
2024-03-04 12:47:24 +0000

    copy paste terminal

--- init.lua
+++ init.lua
@@ -1,4 +1,19 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+-- CTRL+V Copy Paste
 vim.api.nvim_set_keymap("i", "<C-v>", "<C-r>+", { noremap = true, silent = true })
+
+-- CTRL+V Copy Paste for Terminal
+function _G.paste_from_clipboard_in_terminal()
+  local system_clipboard_command = "pbpaste" -- Adjust based on your system, e.g., use 'xclip -o' or 'xsel -b' for Linux
+  local paste_command = vim.fn.system(system_clipboard_command)
+  vim.api.nvim_put({ paste_command }, "", false, true)
+end
+
+vim.api.nvim_set_keymap(
+  "t",
+  "<C-v>",
+  "<Cmd>lua paste_from_clipboard_in_terminal()<CR>",
+  { noremap = true, silent = true }
+)
