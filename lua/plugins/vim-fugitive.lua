1583d81ded3cf16d5397e7eca2ea71de3864e9fb
George <george.vince@hotmail.co.uk>
2024-03-05 10:17:43 +0000

    also while in visual mode

--- lua/plugins/vim-fugitive.lua
+++ lua/plugins/vim-fugitive.lua
@@ -2,7 +2,10 @@ return {
   { "tpope/vim-fugitive", version = "*" },
   {
     "tpope/vim-rhubarb",
+    keys = {
+      { "<leader>gb", "<cmd>GBrowse<cr>", mode = "n", desc = "Open in Browser" }, -- Normal mode
+      { "<leader>gb", ":GBrowse<cr>", mode = "v", desc = "Open in Browser (Visual)" }, -- Visual mode
+    },
     version = "*",
-    keys = { { "<leader>gb", "<cmd>GBrowse<cr>", desc = "Open in Browser" } },
   },
 }
