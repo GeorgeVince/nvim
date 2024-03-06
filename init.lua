22eb6e5f2bdfdd7e3e336f69a5b9cc6ce8bb3772
George <george.vince@hotmail.co.uk>
2024-03-06 16:40:11 +0000

    tidy up

--- init.lua
+++ init.lua
@@ -1,9 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
--- CTRL+V Copy Paste
-vim.api.nvim_set_keymap("i", "<C-v>", "<C-r>+", { noremap = true, silent = true })
-
 -- CTRL+V Copy Paste for Terminal
 function _G.paste_from_clipboard_in_terminal()
   local system_clipboard_command = "pbpaste" -- Adjust based on your system, e.g., use 'xclip -o' or 'xsel -b' for Linux
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- keymaps are automatically loaded on the VeryLazy event
 -- default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- add any additional keymaps here
+
+-- CTRL+V Copy Paste
+vim.api.nvim_set_keymap("i", "<C-v>", "<C-r>+", { noremap = true, silent = true })
