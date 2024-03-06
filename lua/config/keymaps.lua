3482b1ac004650e83d82d09429d3973b7b28a8a4
George <george.vince@hotmail.co.uk>
2024-03-06 16:41:05 +0000

    more tidy

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,20 @@
 
 -- CTRL+V Copy Paste
 vim.api.nvim_set_keymap("i", "<C-v>", "<C-r>+", { noremap = true, silent = true })
+
+-- CTRL+V Copy Paste for Terminal
+function _G.paste_from_clipboard_in_terminal()
+  local system_clipboard_command = "pbpaste" -- Adjust based on your system, e.g., use 'xclip -o' or 'xsel -b' for Linux
+  local paste_command = vim.fn.system(system_clipboard_command)
+  vim.api.nvim_put({ paste_command }, "", false, true)
+end
+
+vim.api.nvim_set_keymap(
+  "t",
+  "<C-v>",
+  "<Cmd>lua paste_from_clipboard_in_terminal()<CR>",
+  { noremap = true, silent = true }
+)
+
+-- Stop Shift+Space from clearing Terminal
+vim.api.nvim_set_keymap("t", "<S-Space>", "<nop>", { noremap = true, silent = true })
