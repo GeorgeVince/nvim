9739df695bbbabff014396b9b1749ab976ed05c2
George <george.vince@hotmail.co.uk>
2024-03-07 21:24:03 +0000

    debug direnv

--- lua/plugins/dap-python.lua
+++ lua/plugins/dap-python.lua
@@ -2,20 +2,20 @@ return {
   {
     "mfussenegger/nvim-dap",
     config = function()
-      -- Your dap configuration here
       local dap = require("dap")
-      dap.configurations.python = {
-        {
-          type = "python", -- the type of debugger to use
-          request = "launch",
-          name = "Python Debug Prefect",
-          program = "${file}", -- This specifies the current file should be used.
-          pythonPath = function()
-            -- Return the path to the python executable you want to use for debugging
-            return "/Users/georgevince/Library/Caches/pypoetry/virtualenvs/flows-Iom276GZ-py3.9/bin/python"
-          end,
-        },
-      }
+
+      local venvPath = os.getenv("VENV")
+      local pythonPath = venvPath and venvPath .. "/bin/python" or "python"
+      -- Ensure the dap.configurations.python table exists
+      dap.configurations.python = dap.configurations.python or {}
+
+      table.insert(dap.configurations.python, {
+        type = "python",
+        request = "launch",
+        name = "Python Debug Poetry",
+        program = "${file}",
+        pythonPath = pythonPath,
+      })
     end,
   },
 }
