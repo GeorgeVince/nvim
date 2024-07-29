42b12c6cf4ab8fae10edd4b048827a44f3eb1263
George <george.vince@hotmail.co.uk>
2024-07-29 13:28:53 +0100

    fix: disable pyright lint

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,15 +1,31 @@
+-- https://www.reddit.com/r/neovim/comments/11k5but/how_to_disable_pyright_diagnostics/
 return {
-  -- disable pyright mason, we just want to use ruff.
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        pyright = {
-          mason = false,
-          autostart = false,
+  "nvim-lspconfig",
+  opts = {
+    ---@type lspconfig.options
+    servers = {
+      pyright = {
+        capabilities = (function()
+          local capabilities = vim.lsp.protocol.make_client_capabilities()
+          capabilities.textDocument.publishDiagnostics.tagSupport.valueSet = { 2 }
+          return capabilities
+        end)(),
+        settings = {
+          python = {
+            analysis = {
+              useLibraryCodeForTypes = true,
+              diagnosticSeverityOverrides = {
+                reportUnusedVariable = "warning", -- or anything
+              },
+              typeCheckingMode = "basic",
+            },
+          },
         },
-        ruff_lsp = {},
-        rust_analyzer = {},
+      },
+      ruff_lsp = {
+        on_attach = function(client, _)
+          client.server_capabilities.hoverProvider = false
+        end,
       },
     },
   },
