5030e2896c5c932607d83b678a4fc2df57ce9965
George Vince <george.vince@hotmail.co.uk>
2025-10-06 11:26:33 +0100

    added: ignore extra file + tidy up telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,18 +1,35 @@
 return {
-  'nvim-telescope/telescope.nvim',
-  branch = '0.1.x', 
+  "nvim-telescope/telescope.nvim",
+  branch = "0.1.x",
   dependencies = {
-    'nvim-lua/plenary.nvim',
-    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+    "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
   },
   config = function()
-    local telescope = require('telescope')
-    local actions = require('telescope.actions')
-    
+    local telescope = require("telescope")
+    local actions = require("telescope.actions")
+
+    local ignore_patterns = {
+      ".git/",
+      "**.venv",
+      "**__pycache__",
+      "**.mypy_cache",
+      "**.pytest_cache",
+    }
+
+    local function build_rg_globs(patterns)
+      local args = {}
+      for _, pattern in ipairs(patterns) do
+        table.insert(args, "--glob")
+        table.insert(args, "!" .. pattern)
+      end
+      return args
+    end
+
     telescope.setup({
       defaults = {
         mappings = {
-         i = {
+          i = {
             ["<C-j>"] = actions.move_selection_next,
             ["<C-k>"] = actions.move_selection_previous,
             ["<esc>"] = actions.close,
@@ -27,7 +44,7 @@ return {
             ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
           },
         },
-        file_ignore_patterns = { "node_modules", ".git/" },
+        file_ignore_patterns = { "node_modules", ".git/", ".pytest_cache" },
         layout_strategy = "horizontal",
         layout_config = {
           horizontal = {
@@ -39,36 +56,42 @@ return {
         find_files = {
           theme = "dropdown",
           previewer = false,
-	  find_command = { "rg", "--files", "--hidden", "--no-ignore-vcs", "--glob", "!.git/", "--glob", "!**.venv", "--glob", "!**__pycache__", "--glob", "!**.mypy_cache" },
+          find_command = vim.list_extend(
+            { "rg", "--files", "--hidden", "--no-ignore-vcs" },
+            build_rg_globs(ignore_patterns)
+          ),
         },
         live_grep = {
           additional_args = function()
-            return { "--hidden", "--no-ignore-vcs", "--glob", "!.git/", "--glob", "!**.venv", "--glob", "!**__pycache__", "--glob", "!**.mypy_cache" }
+            return vim.list_extend(
+              { "--hidden", "--no-ignore-vcs" },
+              build_rg_globs(ignore_patterns)
+            )
           end,
         },
       },
     })
-    
+
     -- Load extensions
-    telescope.load_extension('fzf')
+    telescope.load_extension("fzf")
   end,
   keys = {
     { "<leader><space>", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
     { "<leader>fg", "<cmd>Telescope git_files<cr>", desc = "Git Files" },
-    
+
     { "<leader>sg", "<cmd>Telescope live_grep<cr>", desc = "Live Grep" },
     { "<leader>sw", "<cmd>Telescope grep_string<cr>", desc = "Grep Word" },
-    
+
     { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
-    
+
     { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help Tags" },
     { "<leader>fc", "<cmd>Telescope commands<cr>", desc = "Commands" },
     { "<leader>fk", "<cmd>Telescope keymaps<cr>", desc = "Keymaps" },
-    
+
     { "<leader>gc", "<cmd>Telescope git_commits<cr>", desc = "Git Commits" },
     { "<leader>gb", "<cmd>Telescope git_branches<cr>", desc = "Git Branches" },
     { "<leader>gs", "<cmd>Telescope git_status<cr>", desc = "Git Status" },
-    
+
     { "gd", "<cmd>Telescope lsp_definitions<cr>", desc = "Go to Definition" },
     { "gr", "<cmd>Telescope lsp_references<cr>", desc = "References" },
     { "gi", "<cmd>Telescope lsp_implementations<cr>", desc = "Implementations" },
